907a3adb5ff803a57d1a29b9a9d97412e9c57f26
lemonknowsall <lemonknowsall@gmail.com>
2024-01-13 20:58:39 -0800

    updated setup to get streamlit going again

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-streamlit run Home.py --server.host 0.0.0.0 --server.port 8501 &  # Run Streamlit app in the background
-gunicorn -w 4 -b 0.0.0.0:$PORT app:app --preload  # Run Flask app with Gunicorn
+export HEROKU_PORT=$PORT
+streamlit run Home.py &  # Run Streamlit app in the background
+gunicorn -w 4 -b 0.0.0.0:$PORT app:app  # Run Flask app with Gunicorn
